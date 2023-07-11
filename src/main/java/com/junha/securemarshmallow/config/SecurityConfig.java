b266b39cdf17bad61b01e57ebfe8fd04efbd3587
Wonjune <103029974+one3147@users.noreply.github.com>
2023-07-11 13:39:40 +0900

    Update SecurityConfig.java

--- src/main/java/com/junha/securemarshmallow/config/SecurityConfig.java
+++ src/main/java/com/junha/securemarshmallow/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.junha.securemarshmallow.config;
+import com.junha.securemarshmallow.TokenAuthenticationFilter;
 import com.junha.securemarshmallow.dto.UserAccountDto;
 import com.junha.securemarshmallow.repository.UserAccountRepository;
+import com.junha.securemarshmallow.util.TokenUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +17,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -26,54 +34,64 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final UserAccountRepository userAccountRepository;
 
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+//    @Override
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+    @Autowired
+    private TokenUtils tokenUtils;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(new RequestLengthFilter(), FilterSecurityInterceptor.class)
                 .cors().and()
                 .csrf().disable()
                 .authorizeRequests(auth -> auth
                         .antMatchers("/api/file/**").permitAll()
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .mvcMatchers(HttpMethod.POST, "/api/signup", "/api/login").permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .logout()
                 .logoutSuccessUrl("/");
     }
 
-        private static class RequestLengthFilter extends GenericFilterBean {
+
+    private static class RequestLengthFilter extends OncePerRequestFilter {
         private static final int MAX_REQUEST_LENGTH = 30000;
 
         @Override
-        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-            HttpServletRequest httpRequest = (HttpServletRequest) request;
-            String requestURI = httpRequest.getRequestURI();
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+                throws ServletException, IOException {
+            String requestURI = request.getRequestURI();
 
             if (requestURI.equals("/api/file/upload")) {
-                chain.doFilter(request, response);
+                filterChain.doFilter(request, response);
             } else {
-                if (httpRequest.getContentLength() > MAX_REQUEST_LENGTH) {
-                    ((HttpServletResponse) response).sendError(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE, "Request entity too large");
+                if (request.getContentLength() > MAX_REQUEST_LENGTH) {
+                    response.sendError(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE, "Request entity too large");
                     return;
                 }
-                chain.doFilter(request, response);
+                filterChain.doFilter(request, response);
             }
         }
     }
-
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .addFilterBefore(new TokenAuthenticationFilter(tokenUtils), UsernamePasswordAuthenticationFilter.class);
+//    }
 }
